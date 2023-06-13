a076f0d7ab1e957538a67cd6c378d40db13e50f1
napatsutthitham@gmail.com <sakrinthr240145@gmail.com>
2023-06-13 13:13:02 +0700

    first app

---
+++ bin/dart_week5.dart
@@ -0,0 +1,11 @@
+void main(){
+  print("Assignment operators");
+  print("==========");
+  const int n1 = 11;
+  const int n2 = 5;
+  print("n1+n2 = ${n1 + n2}");
+  print("n1-n2 = ${n1 - n2}");
+  print("n1*n2 = ${n1 * n2}");
+  print("n1/n2 = ${n1 / n2}");
+  print("n1%n2 = ${n1 % n2}");
+}
